d63ea451dea77d6e5636b341a7b05b14d3734a14
poxx1 <julianlastrama@outlook.com>
2020-11-07 18:28:32 -0300

    Sque los using pelotudos que no servian

--- BE_Propiedades/BE_Programs.cs
+++ BE_Propiedades/BE_Programs.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BE_Propiedades
+﻿namespace BE_Propiedades
 {
     public class BE_Programs
     {
--- BE_Propiedades/BE_Servers.cs
+++ BE_Propiedades/BE_Servers.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BE_Propiedades
+﻿namespace BE_Propiedades
 {
     class BE_Servers
     {
