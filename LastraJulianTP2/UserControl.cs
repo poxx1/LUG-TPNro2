a15d7441b70715ab60972b965acfcdeb9e7fa835
poxx1 <julianlastrama@outlook.com>
2020-11-07 19:11:52 -0300

    User control but dont work :S

---
+++ LastraJulianTP2/UserControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LastraJulianTP2
+{
+    public partial class UserControl : Form
+    {
+        public UserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+            var st = new UI_Core();
+            st.Show();
+        }
+    }
+}
