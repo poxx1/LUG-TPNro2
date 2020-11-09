073579a431f24e849a6522819b2ac837934837e7
poxx1 <julianlastrama@outlook.com>
2020-11-09 12:26:15 -0300

    update

---
+++ LastraJulianTP2/UI_Reports.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace LastraJulianTP2
+{
+    public partial class UI_Reports : Form
+    {
+        public UI_Reports()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_Reports_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
